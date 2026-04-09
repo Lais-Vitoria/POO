75740bbc3e4307ac0d6fb3ebd41b7d20fed83a9d
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2026-04-08 21:24:15 -0300

    feat(Aluno.java): Usando herança para criar a classe "Aluno"  a partir da classe "Pessoa" e adicionado um atributo.

---
+++ Basico/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno extends Pessoa{
+
+    private int matricula;
+
+    public int getMatricula(){
+        return this.matricula;
+    }
+
+    public void setMatricula(int matricula){
+        this.setMatricula = matricula;
+    }
+
+}
