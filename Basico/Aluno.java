aa60e5a126a6252c06f59f64fbf4a0f05ce5f083
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2026-04-09 20:04:01 -0300

    refactor(Aluno.java): reutilizando dados de "mostrarDados()" da classe "Pessoa" usando super

--- Basico/Aluno.java
+++ Basico/Aluno.java
@@ -2,12 +2,23 @@ public class Aluno extends Pessoa{
 
     private int matricula;
 
+    public Aluno(String nome, int idade, int matricula){
+        super(nome, idade);
+        this.matricula = matricula;
+    }
+
     public int getMatricula(){
         return this.matricula;
     }
 
     public void setMatricula(int matricula){
-        this.setMatricula = matricula;
+        this.matricula = matricula;
+    }
+    //Polimorfismo usando override
+    @Override
+    public void mostrarDados(){
+        super.mostrarDados();
+        System.out.println("Matricula: " + getMatricula());
     }
 
 }
