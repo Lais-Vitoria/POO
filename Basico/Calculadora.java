097abc3fbf32543bf11baae8633112c7019ddda0
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2026-04-09 20:10:34 -0300

    feat(Calculadora.java): Adicionado a CLasse "Calculadora" com polimorfismo por sobrecarga usando o metodo "soma".

---
+++ Basico/Calculadora.java
@@ -0,0 +1,16 @@
+//Classe com polimorfismo por sobrecarga 
+public class Calculadora{
+
+    public int soma(int a, int b ){
+        return a + b;
+    }
+
+    public double soma( double a, double b){
+        return a + b;
+    }
+
+    public int soma(int a, int b, int c){
+        return a + b + c;
+    }
+
+}
